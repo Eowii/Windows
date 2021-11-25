b31e4bf08995b599a957eccb303850f38ac69fc8
Eowii <andreas.ljunggren93@gmail.com>
2021-11-25 12:50:07 +0100

    Create Network-Drives.ps1

---
+++ Network-Drives.ps1
@@ -0,0 +1,5 @@
+New-PSDrive –Name “V” –PSProvider FileSystem –Root “\\Tower\Photos" –Persist
+New-PSDrive –Name “W” –PSProvider FileSystem –Root “\\Tower\Data" –Persist
+New-PSDrive –Name “X” –PSProvider FileSystem –Root “\\Tower\PDF" –Persist
+New-PSDrive –Name “Y” –PSProvider FileSystem –Root “\\Tower\PlexMedia" –Persist
+New-PSDrive –Name “Z” –PSProvider FileSystem –Root “\\Tower\appdata" –Persist
