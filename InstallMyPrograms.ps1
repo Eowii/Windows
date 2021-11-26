e32f439375e0321a5a6513360bb298c77c7d4a26
Eowii <andreas.ljunggren93@gmail.com>
2021-11-26 11:03:39 +0100

    change install progs

--- InstallMyPrograms.ps1
+++ InstallMyPrograms.ps1
@@ -1,3 +1,3 @@
 	Write-Output "Installing my programs from winget"
 	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Eowii/Windows/main/Programs.json" -OutFile "$env:UserProfile\Programs.json" -ErrorAction SilentlyContinue
-	winget import  $env:UserProfile\Programs.json --silent
+	winget import  $env:UserProfile\Programs.json
