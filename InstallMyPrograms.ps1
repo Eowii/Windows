06b3caedff9cf56fe1da2cfef9dc13303914ae77
Eowii <andreas.ljunggren93@gmail.com>
2021-11-26 21:27:20 +0100

    Update InstallMyPrograms.ps1

--- InstallMyPrograms.ps1
+++ InstallMyPrograms.ps1
@@ -1,6 +1,7 @@
 	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Eowii/Windows/main/Programs.json" -OutFile "$env:UserProfile\Programs.json" -ErrorAction SilentlyContinue
 	winget import  $env:UserProfile\Programs.json
 	rm "$env:UserProfile\Programs.json"
+	rm C:\Users\$env:username\Desktop\*.ps1
 	rm C:\Users\$env:username\Desktop\*.lnk
 	rm C:\Users\Public\Desktop\*.lnk
 	shutdown -r -t 0
